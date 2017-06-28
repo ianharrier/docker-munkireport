efe399b2388de5eeccb3f646af47290bdb973676
Ian Harrier <github@harrier.us>
2017-06-27 19:22:57 -0500

    Ensure config.php is readable

--- images/munkireport/docker-entrypoint.sh
+++ images/munkireport/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+echo "[I] Setting file permissions."
+chown root:root config.php
+chmod +r config.php
+
 if [ "$TIMEZONE" ]; then
     echo "[I] Setting the time zone."
     echo "date.timezone = $TIMEZONE" > /etc/php7/conf.d/timezone.ini
