dd98078a22eb86f8189a29701e6bb4000ae727d9
Ian Harrier <github@harrier.us>
2017-12-30 16:10:21 -0600

    Reorder entrypoint tasks

--- images/munkireport/docker-entrypoint.sh
+++ images/munkireport/docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-echo "[I] Setting file permissions."
-chown root:root config.php
-chmod +r config.php
-
 if [ "$TIMEZONE" ]; then
     echo "[I] Setting the time zone."
     echo "date.timezone = $TIMEZONE" > /etc/php7/conf.d/timezone.ini
 fi
+
+echo "[I] Setting file permissions."
+chown root:root config.php
+chmod +r config.php
 
 echo "[I] Entrypoint tasks complete. Starting MunkiReport."
 exec "$@"
