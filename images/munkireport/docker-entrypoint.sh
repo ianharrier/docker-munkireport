37cb4294c6c1bc752befc8a6e6c722426462ee13
Ian Harrier <github@harrier.us>
2018-11-07 14:21:14 -0600

    Bump MunkiReport version

--- images/munkireport/docker-entrypoint.sh
+++ images/munkireport/docker-entrypoint.sh
@@ -6,6 +6,16 @@ if [ "$TIMEZONE" ]; then
     echo "date.timezone = $TIMEZONE" > /etc/php7/conf.d/timezone.ini
 fi
 
+echo "[I] Waiting for MySQL container to complete initialization tasks."
+DB_READY=false
+while [ "$DB_READY" = "false" ]; do
+    sleep 1
+    nc -z db 3306 &>/dev/null && DB_READY=true || DB_READY=false
+done
+
+echo "[I] Migrating the database."
+php database/migrate.php
+
 echo "[I] Setting file permissions."
 chown root:root config.php
 chmod +r config.php
--- volumes/web/config.template.php
+++ volumes/web/config.template.php
@@ -24,14 +24,16 @@
 	// Leave this set to '' for nicer looking URLs.
 	$conf['index_page'] = '';
 
-	// Leave this set to TRUE for more convenient upgrades to newer versions of MunkiReport.
-	$conf['allow_migrations'] = TRUE;
-
 	// DO NOT MODIFY - MySQL settings are automatically pulled from the .env file.
-	$conf['pdo_dsn'] = "mysql:host=db;dbname=" . getenv('MYSQL_DATABASE');
-	$conf['pdo_user'] = getenv('MYSQL_USER');
-	$conf['pdo_pass'] = getenv('MYSQL_PASSWORD');
-	$conf['pdo_opts'] = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+	$conf['connection'] = [
+	    'driver'    => 'mysql',
+	    'host'      => 'db',
+	    'port'      => 3306,
+	    'database'  => getenv('MYSQL_DATABASE'),
+	    'username'  => getenv('MYSQL_USER'),
+	    'password'  => getenv('MYSQL_PASSWORD'),
+	    'options'   => [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci']
+	];
 
 	// DO NOT MODIFY - Timezone is automatically pulled from the .env file.
 	$conf['timezone'] = @date_default_timezone_get();
